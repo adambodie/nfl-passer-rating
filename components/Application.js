dc2cac038df0d21439b231f1295f57b8d3c0904f
Adam Bodie <aduo3000@yahoo.com>
2017-03-26 16:53:03 -0700

    Added secondary Color property to options array

--- components/Application.js
+++ components/Application.js
@@ -9,44 +9,59 @@ class Application extends React.Component {
     super(props);
     this.state = {
       value: 'nfl',
-      backgroundColor: '#074E68',
-      color: 'black'
+      backgroundColor: '#013A73',
+      primaryColor: '#FFFFFF',
+      secondaryColor: '#DB1B26',
     };
     this.handleValueChange = this.handleValueChange.bind(this);
-    this.handleColorChange = this.handleColorChange.bind(this);
+    this.handlePrimaryColorChange = this.handlePrimaryColorChange.bind(this);
+    this.handleSecondaryColorChange = this.handleSecondaryColorChange.bind(this);
     this.handleBackgroundColorChange = this.handleBackgroundColorChange.bind(this);
   }
 
   handleValueChange(value){
     this.setState({value: value});
   }
-  handleColorChange(color){
-    this.setState({color: color});
+  handlePrimaryColorChange(primaryColor){
+    this.setState({primaryColor: primaryColor});
+  }
+  handleSecondaryColorChange(secondaryColor){
+    this.setState({secondaryColor: secondaryColor});
   }
   handleBackgroundColorChange(backgroundColor){
     this.setState({backgroundColor: backgroundColor});
   }
   render() {
     const colorStyle = {
-      color: this.state.color,
+      color: this.state.primaryColor,
       backgroundColor: this.state.backgroundColor,
     };
+    const secondaryColorStyle = {
+      color: this.state.primaryColor,
+      backgroundColor: this.state.secondaryColor,
+    };
+
     const value = this.state.value;
     return (
       <div style={colorStyle}>
-          <header className="header">
+          <header className="header" style={secondaryColorStyle}>
             <h1 className="headline">NFL Passer Rating</h1>
           </header>
           <div className="row">
             <div className="column medium-3 teams">
               <img className="thumbnail" src={'img/' + value + '.gif'}/>
-              <Team onValueChange={this.handleValueChange} onColorChange={this.handleColorChange} onBackgroundChange={this.handleBackgroundColorChange}/>
+              <Team
+                  onValueChange={this.handleValueChange}
+                  onPrimaryColorChange={this.handlePrimaryColorChange}
+                  onSecondaryColorChange={this.handleSecondaryColorChange}
+                  onBackgroundChange={this.handleBackgroundColorChange}
+                  />
             </div>
             <div className="column medium-9 stats">
               <Rating />
             </div>
           </div>
-          <footer className = "footer" style={colorStyle} >
+          <footer className = "footer" style={secondaryColorStyle} >
             <h3>Adam Bodie <span>©{new Date().getFullYear()}</span></h3>
             </footer>
       </div>
