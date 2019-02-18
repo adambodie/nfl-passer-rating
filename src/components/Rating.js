dd1621ed720f089756dab2b98b17e6107352c897
adambodie <adam.m.bodie@gmail.com>
2019-02-18 10:09:14 -0800

    Added tests for Rating component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,85 +1,86 @@
 import React, { Component } from 'react';
 import { Row, Column } from 'react-foundation';
-const aValue = (completions, attempts) => {
-    if (attempts < 0 || completions < 0) {
-      return "Must be greater than or equal to zero";
-    }
-    if (attempts < completions) {
-      return "More completions than attempts not allowed";
-    } else {
-        let a = ((completions/attempts) - 0.3) * 5;
-        if (a > 2.375) {
-          a = 2.375;
-        }
-        if (a < 0) {
-          a = 0;
-        }
-        return a;
-      }
-  }
 
-  const bValue = (yards, attempts) => {
-    if (yards != 0 && attempts == 0) {
-      return "Need at least one attempt to produce a B value";
-    }
-    let b = ((yards/attempts) - 3) * 0.25;
-    if (b > 2.375) {
-      b = 2.375;
-    }
-    if (b < 0) {
-      b = 0;
-    }
-    return b;
-  }
+export const aValue = (completions, attempts) => {
+	if (attempts < 0 || completions < 0) {
+		return "Must be greater than or equal to zero";
+	}
+	if (attempts < completions) {
+		return "More completions than attempts not allowed";
+	} else {
+		let a = ((completions/attempts) - 0.3) * 5;
+		if (a > 2.375) {
+			a = 2.375;
+		}
+		if (a < 0) {
+			a = 0;
+		}
+		return a;
+	}
+}
 
-  const cValue = (touchdowns, attempts) => {
-    if (attempts < 0 || touchdowns < 0) {
-      return "Must be greater than or equal to zero";
-    }
-    if (touchdowns > attempts) {
-      return "More touchdowns than attempts not allowed";
-    }
-      let c = (touchdowns/attempts) * 20;
-      if (c > 2.375) {
-        c = 2.375;
-      }
-      if (c < 0) {
-        c = 0;
-      }
-      return c;
-    }
+export const bValue = (yards, attempts) => {
+	if (yards != 0 && attempts == 0) {
+		return "Need at least one attempt to produce a B value";
+	}
+	let b = ((yards/attempts) - 3) * 0.25;
+	if (b > 2.375) {
+		b = 2.375;
+	}
+	if (b < 0) {
+		b = 0;
+	}
+	return b;
+}
 
-    const dValue = (interceptions, attempts) => {
-        if (attempts < 0 || interceptions < 0) {
-          return "Must be greater than or equal to zero";
-        }
-        if (interceptions > attempts) {
-          return "More interceptions than attempts not allowed";
-        }    
-        let d = 2.375 - ((interceptions/attempts) * 25);
-        if (d > 2.375) {
-          d = 2.375;
-        }
-        if (d < 0) {
-          d = 0;
-        }
-        return d;
-      }
+export const cValue = (touchdowns, attempts) => {
+	if (attempts < 0 || touchdowns < 0) {
+		return "Must be greater than or equal to zero";
+	}
+	if (touchdowns > attempts) {
+		return "More touchdowns than attempts not allowed";
+	}
+	let c = (touchdowns/attempts) * 20;
+	if (c > 2.375) {
+		c = 2.375;
+	}
+	if (c < 0) {
+		c = 0;
+	}
+	return c;
+}
 
-      const eValue = (completions, attempts, yards, touchdowns, interceptions) => {
-        let a = parseFloat(aValue(completions, attempts));
-        let b = parseFloat(bValue(yards, attempts));
-        let c = parseFloat(cValue(touchdowns, attempts));
-        let d = parseFloat(dValue(interceptions, attempts));
-        let e = ((a + b + c + d)/ 6) * 100;
-        if (e > 158.3) {
-          e = 158.3;
-        }
-        if (e < 0) {
-          e = 0;
-        }
-        return parseFloat(e.toFixed(1));
-      }      
+export const dValue = (interceptions, attempts) => {
+	if (attempts < 0 || interceptions < 0) {
+		return "Must be greater than or equal to zero";
+	}
+	if (interceptions > attempts) {
+		return "More interceptions than attempts not allowed";
+	}    
+	let d = 2.375 - ((interceptions/attempts) * 25);
+    if (d > 2.375) {
+		d = 2.375;
+	}
+	if (d < 0) {
+		d = 0;
+	}
+	return d;
+}
+
+export const eValue = (completions, attempts, yards, touchdowns, interceptions) => {
+	let a = parseFloat(aValue(completions, attempts));
+	let b = parseFloat(bValue(yards, attempts));
+	let c = parseFloat(cValue(touchdowns, attempts));
+	let d = parseFloat(dValue(interceptions, attempts));
+	let e = ((a + b + c + d)/ 6) * 100;
+	if (e > 158.3) {
+		e = 158.3;
+	}
+	if (e < 0) {
+		e = 0;
+	}
+	return parseFloat(e.toFixed(1));
+}
   
 
 export default class Rating extends Component {
---
+++ src/tests/Rating.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Rating from '../components/Rating';
+import { aValue, bValue, cValue, dValue, eValue } from '../components/Rating';
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<Rating />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe('aValue', () => {
+	test('aValue to equal 1', () => {
+		expect(aValue(10, 20)).toBe(1);
+	});
+
+	test('aValue to equal 2.375', () => {
+		expect(aValue(20, 20)).toBe(2.375);
+	});
+
+	test('aValue to equal 0', () => {
+		expect(aValue(0, 1)).toBe(0);
+	});
+});
+
+describe('bValue', () => {
+	test('bValue to equal 1', () => {
+		expect(bValue(280, 40)).toBe(1);
+	});
+
+	test('bValue to equal 2.375', () => {
+		expect(bValue(300, 20)).toBe(2.375);
+	});
+
+	test('bValue to equal 0', () => {
+		expect(bValue(0, 1)).toBe(0);
+	});
+});
+
+describe('cValue', () => {
+	test('cValue to equal 1', () => {
+		expect(cValue(1, 20)).toBe(1);
+	});
+
+	test('cValue to equal 2.375', () => {
+		expect(cValue(1, 1)).toBe(2.375);
+	});
+
+	test('cValue to equal 0', () => {
+		expect(cValue(0, 20)).toBe(0);
+	});
+});
+
+describe('dValue', () => {
+	test('dValue to equal 1', () => {
+		expect(dValue(1, 25)).toBe(1.375);
+	});
+
+	test('dValue to equal 2.375', () => {
+		expect(dValue(1, 1)).toBe(0);
+	});
+
+	test('dValue to equal 0', () => {
+		expect(dValue(0, 20)).toBe(2.375);
+	});
+});
+
+
+describe('eValue', () => {
+	test('passer rating to equal 90.4', () => {
+		expect(eValue(15, 25, 250, 1, 1)).toBe(90.4);
+	});
+
+	test('passer rating to equal 0', () => {
+		expect(eValue(0, 1, 0, 0, 1)).toBe(0);
+	});
+
+	test('passer rating to equal 158.3', () => {
+		expect(eValue(25, 30, 400, 4, 0)).toBe(158.3);
+	});
+});
